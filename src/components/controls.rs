1dbbd896f44c3cecfb3d3c96c67b465faa69a068
William Gough <williamhenrygough@gmail.com>
2019-12-26 16:51:36 -0600

    working state-update

---
+++ src/components/controls.rs
@@ -0,0 +1,79 @@
+use yew::prelude::*;
+
+use crate::Msg as crate_msg;
+
+pub struct Controls {
+    title: String,
+    exits: [u8; 3],
+    onsignal: Option<Callback<crate_msg>>,
+}
+
+pub enum Msg {
+    ButtonPressed(crate_msg),
+}
+
+#[derive(PartialEq, Clone, Properties)]
+pub struct Props {
+    pub exits: [u8; 3],
+    pub onsignal: Option<Callback<crate_msg>>,
+}
+
+impl Default for Props {
+    fn default() -> Self {
+        Self {
+            exits: [0, 0, 0],
+            onsignal: None,
+        }
+    }
+}
+
+impl Component for Controls {
+    type Message = Msg;
+    type Properties = Props;
+
+    fn create(props: Self::Properties, _: ComponentLink<Self>) -> Self {
+        Controls {
+            title: "Controls".into(),
+            exits: props.exits,
+            onsignal: props.onsignal,
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::ButtonPressed(msg) => {
+                if let Some(ref mut callback) = self.onsignal {
+                    callback.emit(msg);
+                }
+            }
+        }
+        false
+    }
+
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        self.exits = props.exits;
+        self.onsignal = props.onsignal;
+        true
+    }
+}
+
+impl Renderable<Controls> for Controls {
+    fn view(&self) -> Html<Self> {
+        let move_button = |target: &u8| {
+            let t = *target;
+            html! {
+                <span class="control-button">
+                    <button onclick=|_| Msg::ButtonPressed(crate_msg::SwitchRoom(t)),>
+                        {&format!("Move to {}", target)}
+                    </button>
+                </span>
+            }
+        };
+        html! {
+            <div class=("container", "container-controls"),>
+                <div class="title",>{&self.title}</div>
+                <div class="exits",>{ for self.exits.iter().map(move_button) }</div>
+            </div>
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,75 @@
 extern crate stdweb;
 
-#[macro_use]
 extern crate yew;
 
 use yew::prelude::*;
 
+mod components;
+
+use self::components::{controls::Controls, messages::Messages, stats::Stats};
+
+fn room_exits(id: u8) -> Option<[u8; 3]> {
+    match id {
+        1 => Some([2, 5, 8]),
+        2 => Some([1, 3, 10]),
+        3 => Some([2, 4, 12]),
+        4 => Some([3, 5, 14]),
+        5 => Some([1, 4, 6]),
+        6 => Some([5, 7, 15]),
+        7 => Some([6, 8, 17]),
+        8 => Some([1, 7, 11]),
+        9 => Some([10, 12, 19]),
+        10 => Some([2, 9, 11]),
+        11 => Some([8, 10, 20]),
+        12 => Some([3, 9, 13]),
+        13 => Some([12, 14, 18]),
+        14 => Some([4, 13, 15]),
+        15 => Some([6, 14, 16]),
+        16 => Some([15, 17, 18]),
+        17 => Some([7, 16, 20]),
+        18 => Some([13, 16, 19]),
+        19 => Some([9, 18, 20]),
+        20 => Some([11, 17, 19]),
+        _ => None,
+    }
+}
+
 pub struct Model {
     arrows: u8,
+    current_room: u8,
+    messages: Vec<String>,
 }
 
 #[derive(Debug, Clone)]
-pub enum Msg {}
+pub enum Msg {
+    SwitchRoom(u8),
+}
 
 impl Component for Model {
-    type Message = ();
+    type Message = Msg;
     type Properties = ();
 
     fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        Model { arrows: 5 }
+        let mut ret = Model {
+            arrows: 5,
+            current_room: 1,
+            messages: Vec::new(),
+        };
+        ret.messages.push(
+            "You've entered a clammy, dark cave, armed with 5 arrows.  You are very cold."
+                .to_string(),
+        );
+        ret
     }
 
-    fn update(&mut self, _msg: Self::Message) -> bool {
-        true
+    fn update(&mut self, msg: Self::Message) -> bool {
+        match msg {
+            Msg::SwitchRoom(target) => {
+                self.current_room = target;
+                self.messages.push(format!("Moved to room {}", target));
+                true
+            }
+        }
     }
 }
 
@@ -31,8 +79,10 @@ impl Renderable<Model> for Model {
             <div class="hunt",>
                 <div class="header",>{"Hunt the Wumpus"}</div>
                 <div class="body",>
-                    <span class="arrows",>{&format!("Arrows: {}", self.arrows)}</span>
+                  <Stats: arrows=self.arrows, current_room=self.current_room,/>
+                  <Controls: exits=room_exits(self.current_room).unwrap(), onsignal=|msg| msg,/>
                 </div>
+                <Messages: messages=&self.messages,/>
             </div>
         }
     }
